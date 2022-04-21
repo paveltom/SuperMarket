3ec88632213ee655d39384c118e3442ee7117c3d
Nadavrav <nadavrav@post.bgu.ac.il>
2022-04-21 11:20:46 +0000

    Upload New File

---
+++ dev/BusinessLogicLayer/Purchase.java
@@ -0,0 +1,10 @@
+package com.company.BusinessLogicLayer;
+
+import java.util.Date;
+import java.util.Map;
+
+public class Purchase {
+    private int purchaseID;
+    private Date purchaseDate;
+    private Map<Integer, Map<Integer, Integer>> products; //<ProductID, <Fixed price, Actual Price (discount?)>>
+}
