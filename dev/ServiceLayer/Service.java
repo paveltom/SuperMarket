99f4406192772a1c638e9793e0c3acf3005db222
Nadavrav <nadavrav@post.bgu.ac.il>
2022-04-22 08:16:08 +0000

    Replace Service.java

--- dev/ServiceLayer/Service.java
+++ dev/ServiceLayer/Service.java
@@ -10,61 +10,69 @@ public class Service
 
     public void getProductsInStock(int branchID){
         //Requirement 2
+        scs.getProductsInStock(branchID);
+
     }
 
     public void getPurchasesHistoryReport(int branchID){
         //Requirement 3
+        scs.getPurchasesHistoryReport(branchID);
     }
 
     public void getCurrentDiscounts(int branchID){
         //Requirement 4
+        scs.getCurrentDiscounts(branchID);
     }
 
     public void getCategories(int branchID){
         //Requirement 5
+        scs.getCategories(branchID);
     }
 
     public void getStockReport(int branchID){
         //Requirement 6
+        scs.getStockReport(branchID);
     }
 
-    public void getStockReportByCategory(int branchID, int CategoryID){
+    public void getStockReportByCategory(int branchID, int categoryID){
         //Requirement 7
+        scs.getStockReportByCategory(branchID,categoryID);
     }
 
     public void getUnusableProductsReport(int branchID){
         //Requirement 8+9
+        scs.getUnusableProductsReport(branchID);
     }
 
     public void insertNewProduct(int branchID, String productName, String productManufacturer, int categoryID, Date supplyTime, int demand){
-
+        scs.insertNewProduct(branchID,productName,productManufacturer,categoryID,supplyTime,demand);
     }
 
     public void insertNewCategory(int branchID,String categoryName){
-
+        scs.insertNewCategory(branchID,categoryName);
     }
 
     public void insertNewDiscount(int branchID, int productID, Date startDate, Date endDate, int amount, Type t){
-
+        scs.insertNewDiscount(branchID, productID, startDate, endDate, amount, t);
     }
 
-    public void insertNewPurchase(int branchID, Date purchaseDate, Map m){
+    //public void insertNewPurchase(int branchID, Date purchaseDate, Map m){
 
-    }
+    //}
 
     public void deleteProduct(int branchID, int productID){
-
+        scs.deleteProduct(branchID, productID);
     }
 
     public void deleteCategory(int branchID, int categoryID){
-
+        scs.deleteCategory(branchID, categoryID);
     }
 
     public void deleteDiscount(int branchID, int discountID){
-
+        scs.deleteDiscount(branchID, discountID);
     }
 
     public void deletePurchase(int branchID, int purchaseID){
-
+        scs.deletePurchase(branchID, purchaseID);
     }
 }
