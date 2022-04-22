d9d7f980208f270834324473c0a484f6395b1107
Nadavrav <nadavrav@post.bgu.ac.il>
2022-04-22 09:35:10 +0000

    Replace Branch.java

--- dev/BusinessLogicLayer/Branch.java
+++ dev/BusinessLogicLayer/Branch.java
@@ -4,4 +4,11 @@ public class Branch {
     private int branchID;
     private String name;
     private StockController stockController;
+
+    Branch(int _branchID,String _name){
+        branchID = _branchID;
+        name = _name;
+        stockController = new StockController();
+    }
+
 }
