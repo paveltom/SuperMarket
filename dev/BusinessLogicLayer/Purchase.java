c3ceb80dc917cf2e5ccda41b392e0aaf724593d4
Nadavrav <nadavrav@post.bgu.ac.il>
2022-04-23 22:40:26 +0000

    Replace Purchase.java

--- dev/BusinessLogicLayer/Purchase.java
+++ dev/BusinessLogicLayer/Purchase.java
@@ -7,4 +7,9 @@ public class Purchase {
     private int purchaseID;
     private Date purchaseDate;
     private Map<Integer, Map<Integer, Integer>> products; //<ProductID, <Fixed price, Actual Price (discount?)>>
+
+    public String toString(){
+        return "Purchase ID : " + purchaseID + " , Purchase Date : " + purchaseDate;
+    }
+
 }
