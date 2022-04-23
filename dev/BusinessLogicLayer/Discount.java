d809fbb9dd2044d5701d85a4f2391b2ee7d03529
Nadavrav <nadavrav@post.bgu.ac.il>
2022-04-23 22:39:13 +0000

    Replace Discount.java

--- dev/BusinessLogicLayer/Discount.java
+++ dev/BusinessLogicLayer/Discount.java
@@ -3,19 +3,25 @@ package com.company.BusinessLogicLayer;
 import java.util.Date;
 import java.util.Map;
 
-public class Discount {
+public class Discount
+{
     private int productID;
     private int discountID;
     private Date discountStartDate;
     private Date discountEndDate;
     private Map<Integer, Type> discount;
 
-    Discount(int _productID,int _discountID,Date _discountStartDate, Date _discountEndDate,Map<Integer,Type> _discount){
+    Discount(int _productID, int _discountID, Date _discountStartDate, Date _discountEndDate, Map<Integer, Type> _discount)
+    {
         productID = _productID;
         discountID = _discountID;
         discountStartDate = _discountStartDate;
         discountEndDate = _discountEndDate;
         discount = _discount;
+
     }
 
+    public String toString(){
+        return "Discount ID : " + discountID + " , Product ID : " + productID + " , Start Date : " + discountStartDate + " , End Date : " + discountEndDate + " , Amount : NEED TO FIX";
+    }
 }
