eaf98e83604469000d35fc84149c4efd4793da76
Mendy Aharonian <mendya@post.bgu.ac.il>
2022-04-25 18:14:10 +0300

    adding get response

--- dev/Service/Response.java
+++ dev/Service/Response.java
@@ -3,13 +3,13 @@ package Service;
 public class Response {
     private String ErrorMessage = null;
 
-    public Response(){
-
-    }
-
+    public Response(){}
     public Response(String msg){
         ErrorMessage = msg;
     }
 
-    public boolean ErrorOccurred(){return ErrorMessage != null;};
+    public boolean ErrorOccurred(){return ErrorMessage != null;}
+
+    public String getErrorMessage(){return ErrorMessage;}
+
 }
