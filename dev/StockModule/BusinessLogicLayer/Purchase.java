2eebc6aababffa24d17e4d2f9037adf3e6a5a505
Nadavrav <nadavrav@post.bgu.ac.il>
2022-05-19 23:22:37 +0000

    Replace Purchase.java

--- dev/StockModule/BusinessLogicLayer/Purchase.java
+++ dev/StockModule/BusinessLogicLayer/Purchase.java
@@ -6,13 +6,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Purchase {
-    private int purchaseID;
+    private static int purchaseID;
     private Date purchaseDate;
     private Map<Integer, Map<Integer, Integer>> products; //<ProductID, <Fixed price, Actual Price (discount?)>>
 
     public Purchase(int _purchaseID, Date _purchaseDate, Map<Integer, Map<Integer, Integer>> _products)
     {
         purchaseID = _purchaseID;
+        purchaseID++;
         purchaseDate = _purchaseDate;
         products = new HashMap<>();
         products.putAll(_products);
