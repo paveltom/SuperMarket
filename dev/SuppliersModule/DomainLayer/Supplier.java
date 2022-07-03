5164c94ec38db1cc215e2e25ff329aa13cd7e98e
Mendy Aharonian <mendya@post.bgu.ac.il>
2022-07-03 21:44:25 +0300

    removing todo

--- dev/SuppliersModule/DomainLayer/Supplier.java
+++ dev/SuppliersModule/DomainLayer/Supplier.java
@@ -128,7 +128,7 @@ public class Supplier {
         if(cp != null && !cp.isEmpty()) {
             Map<String, Integer> prodQuantities = oc.orderPeriodic(cp, contract.getPeriodicOrderInterval());
             String phone = contacts.entrySet().stream().findFirst().get().getValue();
-            Order order = new Order(sId+LocalDate.now(), sId, name, address, LocalDate.now(), phone); //todo: change date format here or in db
+            Order order = new Order(sId+LocalDate.now(), sId, name, address, LocalDate.now(), phone);
             for (Map.Entry<String, Integer> entry : prodQuantities.entrySet()) {
                 String pId = entry.getKey();
                 int amount = entry.getValue();
